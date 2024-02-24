33c39a0472245c16b9e56bea92bf234846041949
Risad212 <hafez.risad@gmail.com>
2024-02-24 19:59:47 +0600

    done

--- src/Layouts/AdminSidebar/AdminSidebar.jsx
+++ src/Layouts/AdminSidebar/AdminSidebar.jsx
@@ -3,40 +3,52 @@ import { Link } from 'react-router-dom';
 import './adminsidebar.css';
 
 const AdminSidebar = () => {
-    const [toggle, setToggle] = useState(false)
-    const [dropdown, setDropdown] = useState({height: '0'})
-
-   const toggleMenu = () => {
-      setToggle(!toggle)
-      if(toggle){
-        setDropdown({height: '0'})
-      }else{
-        setDropdown({height: 'auto'})
-      }
-   }
     return (
         <div className='adminSidebar'>
             <div className="logo">
                 <h3>Dev Store</h3>
             </div>
-            <ul>
-                <Link to="/admin"><li><i class="fa-solid fa-house"></i> <span className='adminNavItem'>Home</span></li></Link>
-                <li className='dropdown' onClick={() => toggleMenu()}>
-                    <i class="fa-solid fa-cart-shopping"></i> Products {toggle? <i class="fa-solid fa-angle-down"></i>: <i class="fa-solid fa-angle-up"></i>}
-                    <ul className="dropdownmenu" style={dropdown}>
-                        <li><Link className="dropdown-item" href="#">Add product</Link></li>
-                        <li><Link className="dropdown-item" href="#">Edit product</Link></li>
-                        <li><Link className="dropdown-item" href="#">All product</Link></li>
-                    </ul>
-                </li>
-                <Link to="#"><li><i class="fa-solid fa-file-invoice"></i> invoice</li></Link>
-                <Link to="#"><li><i class="fa-solid fa-envelope"></i> Email</li></Link>
-                <Link to="#"><li><i class="fa-solid fa-blog"></i> Blog</li></Link>
-                <Link to="#"><li><i class="fa-solid fa-chart-simple"></i> Chart</li></Link>
-                <Link to="#"><li><i class="fa-solid fa-user"></i> User</li></Link>
-            </ul>
+            <div className='siderMenuList'>
+                <div class="nav-item">
+                    <Link href="#" className="nav-link">
+                        <span className="sidebar-icon"><i class="fa-solid fa-house"></i>
+                        </span>
+                        <span className="sidebar-text">Home</span>
+                    </Link>
+                </div>
+                <div class="nav-item">
+                    <Link href="#" class="nav-link"><span><span class="sidebar-icon"><i class="fa-solid fa-cart-shopping"></i></span><span class="sidebar-text">Product</span></span>
+                    </Link>
+                </div>
+                <div class="nav-item">
+                    <Link href="#" class="nav-link"><span><span class="sidebar-icon"><i class="fa-solid fa-hand-holding-dollar"></i></span><span class="sidebar-text">Transactions</span></span>
+                    </Link>
+                </div>
+                <div class="nav-item">
+                    <Link href="#" class="nav-link"><span><span class="sidebar-icon"><i class="fa-solid fa-file-invoice"></i></span><span class="sidebar-text">invoice</span></span>
+                    </Link>
+                </div>
+                <div class="nav-item">
+                    <Link href="#" class="nav-link"><span><span class="sidebar-icon"><i class="fa-solid fa-envelope"></i></span><span class="sidebar-text">Email</span></span>
+                    </Link>
+                </div>
+                <div class="nav-item">
+                    <Link href="#" class="nav-link"><span><span class="sidebar-icon"><i class="fa-solid fa-blog"></i></span><span class="sidebar-text">Blog</span></span>
+                    </Link>
+                </div>
+                <div class="nav-item">
+                    <Link href="#" class="nav-link"><span><span class="sidebar-icon"><i class="fa-solid fa-chart-simple"></i></span><span class="sidebar-text">Blog</span></span>
+                    </Link>
+                </div>
+                <div class="nav-item">
+                    <Link href="#" class="nav-link"><span><span class="sidebar-icon"><i class="fa-solid fa-user"></i></span><span class="sidebar-text">User</span></span>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default AdminSidebar;
+
+
